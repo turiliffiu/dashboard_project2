e40b1e702242d0a2431f5a9f254219a37be264e4
turiliffiu <52448161+turiliffiu@users.noreply.github.com>
2025-11-02 18:47:18 +0100

    Update settings.py

--- dashboard_project/settings.py
+++ dashboard_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
