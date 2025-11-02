caab7ae5c42a9037bb65e4a8a0844228ed92c1ab
turiliffiu <52448161+turiliffiu@users.noreply.github.com>
2025-11-02 17:51:27 +0100

    Update settings.py

--- dashboard_project/settings.py
+++ dashboard_project/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = env('STATIC_URL')
-#STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = env('STATIC_ROOT')
 
 # Configurazione per la cartella dei file di procedure
