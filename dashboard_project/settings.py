7e80ba9526651d78153de903cad03294aac83e23
turiliffiu <52448161+turiliffiu@users.noreply.github.com>
2025-11-02 16:34:32 +0100

    Update settings.py

--- dashboard_project/settings.py
+++ dashboard_project/settings.py
@@ -14,12 +14,11 @@ from pathlib import Path
 import environ
 import os
 
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
